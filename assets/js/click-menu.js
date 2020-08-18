7566911dd302e45882f2ca00ea910fb900f3860f
enenddhsl <59918259+enenddhsl@users.noreply.github.com>
2020-08-18 14:56:20 +0900

    20200818_on

--- assets/js/click-menu.js
+++ assets/js/click-menu.js
@@ -7,6 +7,7 @@ mobileMenu += '      </span>';
 mobileMenu += '  </button>';
 mobileMenu += '  <div class="mobile-menu-list">';
 mobileMenu += '<ul>';
+mobileMenu += '      <li><a href="./package.html">택배문의</a></li>';
 mobileMenu += '      <li><a href="./branch.html">지점안내</a></li>';
 mobileMenu += '      <li><a href="./inquiry.html">가맹문의</a></li>';
 mobileMenu += '    </ul>';
