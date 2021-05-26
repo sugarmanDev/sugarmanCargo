8bbf391147a3e61e30d672e928cfceabe96df70c
enenddhsl <59918259+enenddhsl@users.noreply.github.com>
2021-05-26 18:31:58 +0900

    20210526_on

--- assets/js/click-menu.js
+++ assets/js/click-menu.js
@@ -43,7 +43,7 @@ var targetDomain = '';
 console.log(getDomain);
 
 if (getDomain == 'sugarmancargo.com' || getDomain == 'sugarmandev.github.io') {
-  targetDomain = 'http://54.180.162.123/cargo';
+  targetDomain = 'http://sugarmango.co.kr/cargo';
 } else {
   targetDomain = 'http://localhost/cargo';
 }
