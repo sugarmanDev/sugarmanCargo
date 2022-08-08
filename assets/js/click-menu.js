4ac94638a863a9813995dfa68f7272c3ff6dd443
haeun1049 <50644565+haeun1049@users.noreply.github.com>
2022-08-08 13:09:30 +0900

    220808_haeun

--- assets/js/click-menu.js
+++ assets/js/click-menu.js
@@ -4,45 +4,45 @@ mobileMenu += '  <button class="close-btn toggle-mobile-menu">';
 // mobileMenu += '    <span class="material-icons">';
 // mobileMenu += '      clear';
 // mobileMenu += '      </span>';
-mobileMenu += '  </button>';
+mobileMenu += "  </button>";
 mobileMenu += '  <div class="mobile-menu-list">';
-mobileMenu += '<ul>';
+mobileMenu += "<ul>";
 mobileMenu += '      <li><a href="./branch_list.html">지점찾기</a></li>';
 mobileMenu += '      <li><a href="./franchise.html">가맹안내</a></li>';
-mobileMenu += '    </ul>';
+mobileMenu += "    </ul>";
 mobileMenu += '    <a href="./visit.html">';
 mobileMenu += '      <button type="button">';
-mobileMenu += '        방문예약';
-mobileMenu += '      </button>';
-mobileMenu += '    </a>';
-mobileMenu += '  </div>';
-mobileMenu += '</div>';
-mobileMenu += '</div>';
+mobileMenu += "        방문예약";
+mobileMenu += "      </button>";
+mobileMenu += "    </a>";
+mobileMenu += "  </div>";
+mobileMenu += "</div>";
+mobileMenu += "</div>";
 
-$(document).on('click', '.toggle-mobile-menu', function() {
-  $('.mobile-menu').toggle();
-  $('.web-popup').hide();
-  $('html').toggleClass('prevent-scroll')
-  $('body').toggleClass('prevent-scroll')
+$(document).on("click", ".toggle-mobile-menu", function () {
+    $(".mobile-menu").toggle();
+    $(".web-popup").hide();
+    $("html").toggleClass("prevent-scroll");
+    $("body").toggleClass("prevent-scroll");
 });
 
-$(document).ready(function() {
-  $('body').append(mobileMenu);
-})
+$(document).ready(function () {
+    $("body").append(mobileMenu);
+});
 
-$(document).on('click', '.event-banner-btn', function() {
-  $('.event-banner').remove();
-  $('.top-nav').css('top', '0px')
+$(document).on("click", ".event-banner-btn", function () {
+    $(".event-banner").remove();
+    $(".top-nav").css("top", "0px");
 });
 
 var getDomain = window.location.hostname;
 
-var targetDomain = '';
+var targetDomain = "";
 
 console.log(getDomain);
 
-if (getDomain == 'sugarmancargo.com' || getDomain == 'sugarmandev.github.io') {
-  targetDomain = 'https://sugarmango.co.kr/cargo';
+if (getDomain == "sugarmancargo.com" || getDomain == "sugarmandev.github.io") {
+    targetDomain = "https://sugarmancargo.com/cargo";
 } else {
-  targetDomain = 'http://localhost/cargo';
+    targetDomain = "http://localhost/cargo";
 }
