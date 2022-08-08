71267a7403a80ba754cb8758dd5933f5ab029aec
haeun1049 <50644565+haeun1049@users.noreply.github.com>
2022-08-08 14:51:21 +0900

    220808_haeun

--- assets/js/click-menu.js
+++ assets/js/click-menu.js
@@ -42,7 +42,7 @@ var targetDomain = "";
 console.log(getDomain);
 
 if (getDomain == "sugarmancargo.com" || getDomain == "sugarmandev.github.io") {
-    targetDomain = "https://salk.co.kr/cargo";
+    targetDomain = "https://sugarmancargo.co.kr/cargo";
 } else {
     targetDomain = "http://localhost/cargo";
 }
