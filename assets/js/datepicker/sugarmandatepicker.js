7ad0256eaf5978d820a18be7a2db081aee1e8391
으니 <72530436+eundol0519@users.noreply.github.com>
2022-11-16 13:30:12 +0900

    20221116_eunhui

--- assets/js/datepicker/sugarmandatepicker.js
+++ assets/js/datepicker/sugarmandatepicker.js
@@ -1,9 +1,24 @@
 // Make Sunday and Saturday disabled
 
-
-
-var solar_holidays = ["0101", "0301", "0505", "0606", "0717", '0815', "1003", "1225"]; //양력휴일
-var set_luna_holidays = ["1231", "0101", "0102", "0408", "0814", "0815", "0816"]; //음력휴일
+var solar_holidays = [
+  "0101",
+  "0301",
+  "0505",
+  "0606",
+  "0717",
+  "0815",
+  "1003",
+  "1225",
+]; //양력휴일
+var set_luna_holidays = [
+  "1231",
+  "0101",
+  "0102",
+  "0408",
+  "0814",
+  "0815",
+  "0816",
+]; //음력휴일
 var luna_holidays = [];
 
 var added_holidays = []; //추가 휴일
@@ -12,22 +27,20 @@ var current_year = new Date().getFullYear();
 set_luna_holidays.forEach(function (value) {
   var set_holiday = calc_lunar(current_year + value);
   luna_holidays.push(set_holiday);
-})
-
+});
 
 function set_date_picker(selector) {
+  var minDate = new Date();
+  minDate.setDate(minDate.getDate()); // 이전 날짜 비활성화
 
   $(selector).datepicker({
-    language: 'ko',
+    language: "ko",
     firstDay: 1,
-    dateFormat: 'yyyy-mm-dd',
-
-
+    dateFormat: "yyyy-mm-dd",
+    minDate: minDate,
 
     onRenderCell: function (date, cellType) {
-
-
-      if (cellType == 'day') {
+      if (cellType == "day") {
         var isDisabled = false;
 
         var day = date.getDay();
@@ -35,32 +48,26 @@ function set_date_picker(selector) {
           isDisabled = true;
         } else {
           var year = date.getFullYear();
-          var month = (1 + date.getMonth()); //M
-          month = month >= 10 ? month : '0' + month; //month 두자리로 저장
+          var month = 1 + date.getMonth(); //M
+          month = month >= 10 ? month : "0" + month; //month 두자리로 저장
 
           var day = date.getDate(); //d
-          day = day >= 10 ? day : '0' + day; //day
+          day = day >= 10 ? day : "0" + day; //day
 
           var set_disabled = month + day;
 
-
-          var get_holiday1 = solar_holidays.indexOf(set_disabled)
-          var get_holiday2 = luna_holidays.indexOf(set_disabled)
-          var get_holiday3 = added_holidays.indexOf(set_disabled)
-
-
-
+          var get_holiday1 = solar_holidays.indexOf(set_disabled);
+          var get_holiday2 = luna_holidays.indexOf(set_disabled);
+          var get_holiday3 = added_holidays.indexOf(set_disabled);
 
           if (get_holiday1 > -1 || get_holiday2 > -1 || get_holiday3 > -1) {
             isDisabled = true;
           }
-
-
         }
         return {
-          disabled: isDisabled
-        }
+          disabled: isDisabled,
+        };
       }
-    }
-  })
+    },
+  });
 }
