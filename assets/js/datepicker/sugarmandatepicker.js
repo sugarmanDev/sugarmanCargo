42ca81c28344a3b2fef07c6c46e676d71f2b06aa
오은희 <72530436+eundol0519@users.noreply.github.com>
2023-01-27 15:56:33 +0900

    Revert "20230127_eunhui"
    This reverts commit 40fb4e78de421fc899a700166a425e4668008256.

--- assets/js/datepicker/sugarmandatepicker.js
+++ assets/js/datepicker/sugarmandatepicker.js
@@ -61,6 +61,7 @@ function set_date_picker(selector, type) {
           let get_holiday2 = luna_holidays.includes(set_disabled);
           let get_holiday3 = added_holidays.includes(set_disabled);
 
+          console.log(set_disabled);
           if (get_holiday1 || get_holiday2 || get_holiday3) {
             isDisabled = false;
           }
